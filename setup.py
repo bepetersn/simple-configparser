57524da66dea1c2dd406b920389277bdea096a0c
Brian Peterson <bepetersn@gmail.com>
2015-12-11 18:24:02 -0700

    Up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools  # this import is needed so that some options and commands wor
 
 setup(
     name='simple-configparser',
-    version='0.1.2.0',
+    version='0.1.3.0',
     author='Brian E. Peterson',
     author_email='bepetersondev@gmail.com',
     url='https://github.com/bepetersn/simple-configparser',
